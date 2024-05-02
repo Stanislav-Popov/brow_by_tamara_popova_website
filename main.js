b92dad68af0ebcd794653f2916545ed1fd98ec07
Stanislav-Popov <104524210+Stanislav-Popov@users.noreply.github.com>
2024-05-02 17:41:31 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+/** @format */
+
+document.addEventListener("DOMContentLoaded", function () {
+    document
+        .getElementById("burger-btn")
+        .addEventListener("click", function () {
+            console.log(document.querySelector(".navbar"))
+            document.querySelector("nav").classList.toggle("open")
+        })
+})  
 
